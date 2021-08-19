aed26a67a0cb0cb344f90c711b3307404e69912e
hashem <abobssam991@gmail.com>
2021-08-20 00:41:45 +0300

    App done

--- app/src/main/java/com/example/chatapp/Profile.java
+++ app/src/main/java/com/example/chatapp/Profile.java
@@ -1,6 +1,7 @@
 package com.example.chatapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -116,5 +118,18 @@ public class Profile extends AppCompatActivity {
         startActivity(intent);
         firebaseAuth.signOut();
         Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show();
+    }
+    public void GoToCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 & resultCode == RESULT_OK & data != null){
+            SelectRecent selectRecent = new SelectRecent(data);
+            selectRecent.show(getSupportFragmentManager(),"SelectRecent");
+        }
     }
 }
